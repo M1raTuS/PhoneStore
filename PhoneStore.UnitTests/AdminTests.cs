fed9a5e27b2993adc24e89a4a84aaf1fa0e9c86b
Sergey <Dnepr.IP@mail.ru>
2020-06-26 12:42:24 +0300

    Added admin panel view

---
+++ PhoneStore.UnitTests/AdminTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PhoneStore.Domain.Abstract;
+using PhoneStore.Domain.Entities;
+using PhoneStore.WebUI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PhoneStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Phones()
+        {
+            //Arrange
+            Mock<IPhoneRepository> mock = new Mock<IPhoneRepository>();
+            mock.Setup(m => m.Phones).Returns(new List<Phone>
+            {
+                new Phone { PhoneId = 1, Name = "Phone1"},
+                new Phone { PhoneId = 2, Name = "Phone2"},
+                new Phone { PhoneId = 3, Name = "Phone3"},
+                new Phone { PhoneId = 4, Name = "Phone4"},
+                new Phone { PhoneId = 5, Name = "Phone5"}
+            });
+
+            AdminController controller = new AdminController(mock.Object);
+
+            //Act
+            List<Phone> result = ((IEnumerable<Phone>)controller.Index().ViewData.Model).ToList();
+
+            //Assert
+            Assert.AreEqual(result.Count(), 5);
+            Assert.AreEqual("Phone1", result[0].Name);
+            Assert.AreEqual("Phone2", result[1].Name);
+            Assert.AreEqual("Phone3", result[2].Name);
+        }
+    }
+}
---
+++ PhoneStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using PhoneStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace PhoneStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IPhoneRepository repository;
+        public AdminController(IPhoneRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Phones);
+        }
+    }
+}
